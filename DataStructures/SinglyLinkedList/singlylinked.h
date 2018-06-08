6da0e876c5253fb209d5209c71c78e503a701ab3
William Walcher <r0u1b@lulu.ugrad.cs.ubc.ca>
2018-06-08 01:52:11 -0700

    Tested and working Singly Linked List

--- DataStructures/SinglyLinkedList/singlylinked.h
+++ DataStructures/SinglyLinkedList/singlylinked.h
@@ -1,24 +1,14 @@
-#ifndef SINGLYLINKED_H
-#define SINGLYLINKED_H
 
-class singlylinked {
-
-    struct Node {
-        int data;
-        Node* next;
-    };
-
-    public:
-        void insert(int pos, int data);
-        void insertFront(int data);
-        void insertBack(int data);
-        void remove(int data);
-        void removeFront();
-        void removeBack();
-    private:
-        int length;
-        Node* head;
-        Node* tail;
+struct Node {
+   int data;
+   Node* next;
 };
 
-#endif
+   
+void insert(int pos, int data);
+void insertFront(int data);
+void insertBack(int data);
+void remove(int data);
+void removeFront();
+void removeBack();
+int size();
